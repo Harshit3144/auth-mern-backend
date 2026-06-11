6dc1fb11ffd73fcf39a9fd991d570c8dd775f32e
Harshit Mayank <harshitmayank335@gmail.com>
2026-06-11 18:58:29 +0530

    Fix body-parser import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
